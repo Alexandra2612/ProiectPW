7139072132e310e25f65ad41cd221d1f774e0a41
Alexandra2612 <alexandra.hertanu@yahoo.com>
2020-11-30 15:25:48 +0200

    Added announcements implementation

--- announcements.php
+++ announcements.php
@@ -8,6 +8,15 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+  $( function() {
+    $( "#accordion" ).accordion();
+  } );
+  </script>
 </head>
 <body style="background-color:azure">
   <header>
@@ -41,8 +50,43 @@
     </ul>
   </div>
 </nav>
-
       <br>
+     <h1 style="margin-left:1%;margin-right:1%">Intrebari frecvente</h1>
+      <br>
+	  <img style="margin-left:1%;margin-right:1%" src="images/pozaanunturi.jpg" alt="Poza intrebari frecvente" width="400" height="300" />
+	  <br>
+	  <br>
+	  <div id="accordion" style="margin-left:1%;margin-right:1%,magin-top:5%" >
+
+ <h3>Ce noutati sunt in anul 2020?</h3>
+  <div>
+    <p>
+     Noutatile in anul 2020 sunt noile destinatii turistice.Irlanda,Spania,Portugalia si Cehia.Pachet cu autocar sau avion din Timisoara.Sejururi chiar si de 7 nopti.
+    </p>
+  </div>
+  <h3>Taxele de aeroport sunt incluse?</h3>
+  <div>
+    <p>
+     Nu,acestea nu sunt incluse in pret.Achizitiile de bilete de avion si potentialele costuri cu bagajul se platesc de fiecare in parte la compania aeriana alatui de care vom calatori.
+    </p>
+  </div>
+  <h3>Ce alte taxe suplimentare sunt in plus?</h3>
+  <div>
+    <p>
+     Asigurarea medicala si storno se achita in plus (nu este obligatorie, dar este recomandata), cu execptia anumitor plecari pe Turcia si Creta, unde cele doua tipuri de asigurari sunt incluse in pretul afisat.
+     De asemenea, se mai achita si taxa de statiune pe Grecia; taxa de viza, acolo unde este cazul (de exemplu, Egipt).
+     Daca plecarile sunt din Budapesta, se poate oferi transfer pana la aeroport cu 150 lei/persoana/sens. 
+    </p>
+  </div>
+  <h3>Care sunt modalitatile de plata?</h3>
+  <div>
+    <p>
+     Se poate plati cash – euro sau lei; transfer bancar – euro sau lei; sau cu cardul – in lei.
+    </p>
+  </div>
+</div>
+ 
+ 
 	<br>
 	<br>
     <footer class="the-footer">
